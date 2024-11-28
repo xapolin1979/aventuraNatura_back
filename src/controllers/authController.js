8d5977ddd17cd1ac3f6c333ffd8998d703ff9670
Eloi Ginesti <ginesti1979@gmail.com>
2024-11-28 14:04:17 +0100

    4.0

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -40,7 +40,7 @@ export const register = async (req, res) => {
     const token = serialize('token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 60 * 60 * 24 * 30,
       path: '/',
     });
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
     const token = serialize('token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 60 * 60 * 24 * 30,
       path: '/',
     });
@@ -236,7 +236,7 @@ export const changePassword = async (req, res) => {
     const token_jwt = serialize('token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 60 * 60 * 24 * 30,
       path: '/',
     });
@@ -272,7 +272,7 @@ export const logout = async (req, res) => {
   const token = serialize('token', null, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
     maxAge: -1,
     path: '/',
   });
